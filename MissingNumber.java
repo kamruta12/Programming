cc2d4076040d605166742c8a0b69eef1a35120e6
Amruta <amruta.kulkarni12@gmail.com>
2025-01-29 17:25:26 +0530

    Changes to include comment

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,8 @@
+/**
+ This class contains solution to LeetCode 268 problem - Missing Number. Problem description is - 
+ Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ The implemented approach has Time complexity = O(n) and Space Complexity = O(1)
+**/
 public class LeetCode268 {
     public int missingNumber(int[] nums) {
         int n = nums.length;
